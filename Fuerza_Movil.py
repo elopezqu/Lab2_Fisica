3a854e755a46f4f18b46717fcc4cb5de56b5ac89
Edson Lopez Quispe <elopezqu@unsa.edu.pe>
2024-05-08 03:20:53 -0500

    Se grafica el cambio de velocidad

--- Fuerza_Movil.py
+++ Fuerza_Movil.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 print("1) Este programa encontrara la fuerza de un movil en movimiento")
 print("2) Este programa graficara el cambio de velocidad de un movil")
 print("Datos del cuerpo --->")
@@ -6,4 +8,10 @@ veloInicial = float(input("Velocidad Iniciald(m/s):  "))
 veloFinal = float(input("Velocidad Final(m/s):  "))
 tiempo =  float(input("Tiempo(s): "))
 
-print("La fuerza que describe el movil es: ",masa*((veloFinal-veloInicial)/tiempo),"N")
+print("La fuerza que describe el movil es: ",abs(masa*((veloFinal-veloInicial)/tiempo)),"N")
+
+plt.plot([0,tiempo],[veloInicial,veloFinal])
+plt.title('Cambio de velocidad')
+plt.xlabel('Tiempo(s)')
+plt.ylabel('Velocidad(m/s)')
+plt.show()
