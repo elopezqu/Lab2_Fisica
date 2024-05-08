0f168e2189d2e05c503f0dc599ab2ed26b0a81a3
Edson Lopez Quispe <elopezqu@unsa.edu.pe>
2024-05-08 03:12:53 -0500

    Se crea un nuevo programa para el punto B, y las variables necesarias

---
+++ Fuerza_Movil.py
@@ -0,0 +1,7 @@
+print("1) Este programa encontrara la fuerza de un movil en movimiento")
+print("2) Este programa graficara el cambio de velocidad de un movil")
+print("Datos del cuerpo --->")
+masa = float(input("Masa(kg): "))
+veloInicial = float(input("Velocidad Iniciald(m/s):  "))
+veloFinal = float(input("Velocidad Final(m/s):  "))
+tiempo =  float(input("Tiempo(s): "))
